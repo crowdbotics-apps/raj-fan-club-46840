16d791b03c7165517035215745552cf1d3722569
Crowdbotics <team@crowdbotics.com>
2024-03-06 16:39:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "raj-fan-club-46840",
+  url: "https://raj-fan-club-46840.botics.co",
+  api: "https://raj-fan-club-46840.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
